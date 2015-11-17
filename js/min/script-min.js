0947a7c4e076432f74e69d95ec80bc665ee7cba1
miles retuta <milesretuta@gmail.com>
2015-11-17 12:14:20 -0800

    og:image tag update

--- js/min/script-min.js
+++ js/min/script-min.js
@@ -1,1 +1,99 @@
-$(document).ready(function(){$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")===this.pathname.replace(/^\//,"")&&location.hostname===this.hostname){var n=$(this.hash);if(n=n.length?n:$("[name="+this.hash.slice(1)+"]"),n.length)return $("html,body").animate({scrollTop:n.offset().top},1e3),!1}})});var n=$(".main-nav"),o="main-nav-scrolled",e=$("#homepage").height();$(window).scroll(function(){console.log($(this).scrollTop(),e),$(this).scrollTop()>e?n.addClass(o):n.removeClass(o)}),$("#clock").countdown("2015/09/28",function(n){var o=$(this).html(n.strftime("<span>%D</span> days <span>%H</span> hours <span>%M</span> minutes <span>%S</span> seconds"))}),$(document).ready(function(){$(".bxslider").bxSlider()}),$(document).ready(function(){$(".bm-cover").click(function(){$(".bm-cover").fadeOut("500")}),$(".gm-cover").click(function(){$(".gm-cover").fadeOut("500")})}),$(document).ready(function(){$(".owl-carousel").owlCarousel()})});
+$(document).ready(function(){
+  // SLIDESHOW
+  cbpBGSlideshow.init();
+
+  $('body').flowtype({
+    minimum   : 500,
+    maximum   : 1200,
+    minFont   : 12,
+    maxFont   : 20,
+    fontRatio : 10
+  });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+        }
+      }
+    });
+
+  // NAVIGATION SCROLL
+  var  mn = $(".main-nav"),
+  mns = "main-nav-scrolled",
+  hdr = $('#homepage').height();
+
+  $(window).scroll(function() {
+    
+      if( $(this).scrollTop() > hdr ) {
+        mn.addClass(mns);
+      } 
+      else {
+      mn.removeClass(mns);
+      }
+  });  
+
+
+  // COUNTDOWN
+
+   $('#clock').countdown('2015/09/28', function(event) {
+    var $this = $(this).html(event.strftime(
+      '<span>%D</span> days '
+      + '<span>%H</span> hours '
+      + '<span>%M</span> minutes '
+      + '<span>%S</span> seconds'
+      ));
+    }); 
+
+	$('.bxslider').bxSlider();
+
+	$('.bm-cover').click(function() {
+      $('.bm-cover').fadeOut('500');
+    });
+
+  $('.gm-cover').click(function() {
+    $('.gm-cover').fadeOut('500');
+  });
+
+
+  $(".owl-carousel").owlCarousel();
+
+  //make ajax request to instagram, get JSON, dynamically build HTML
+
+	ajaxCall("https://api.instagram.com/v1/tags/happilyeverabrenica/media/recent");
+
+});
+
+
+function processResponse(data){
+	console.log(data);
+	var igFeed = data;
+
+	for(var i = 0; i < 20; i++){
+		$("#ig-feed").append("<div><a target='_blank' href='"+igFeed.data[i].link+"'><img class='ig-photo' src='"+igFeed.data[i].images.standard_resolution.url+"'/></a></div>");
+	}
+
+	// if next_url exists do another ajax call with THAT URL and then process that data
+	if (igFeed.pagination.next_url != "") {
+		ajaxCall(igFeed.pagination.next_url);
+	}
+}
+
+function ajaxCall(url){
+
+	$.ajax({
+		type: "GET",
+		url: url,
+		data: "access_token=992364.6d08390.2e471e93a5ce42eeb15ab4669a58ca11",
+		dataType: "jsonp",
+		success: function(serverResponse){
+			processResponse(serverResponse);
+		}
+	});
+}
+
--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+  // SLIDESHOW
+  cbpBGSlideshow.init();
 
-  
-//NAV LINKS
-  $(function() {
+  $('body').flowtype({
+    minimum   : 500,
+    maximum   : 1200,
+    minFont   : 12,
+    maxFont   : 20,
+    fontRatio : 10
+  });
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       var target = $(this.hash);
@@ -15,24 +22,24 @@ $(document).ready(function(){
         }
       }
     });
-  });
 
-// NAVIGATION SCROLL
+  // NAVIGATION SCROLL
   var  mn = $(".main-nav"),
   mns = "main-nav-scrolled",
   hdr = $('#homepage').height();
 
   $(window).scroll(function() {
-    console.log($(this).scrollTop(), hdr);
+    
       if( $(this).scrollTop() > hdr ) {
         mn.addClass(mns);
       } 
       else {
       mn.removeClass(mns);
       }
-  });
+  });  
 
-// COUNTDOWN
+
+  // COUNTDOWN
 
    $('#clock').countdown('2015/09/28', function(event) {
     var $this = $(this).html(event.strftime(
@@ -43,25 +50,49 @@ $(document).ready(function(){
       ));
     }); 
 
-// HOMEPAGE SLIDESHOW
+	$('.bxslider').bxSlider();
 
-  $(document).ready(function(){
-    $('.bxslider').bxSlider();
-  });
-
-// ENTOURAGE
-  $(document).ready(function() {
-    $('.bm-cover').click(function() {
+	$('.bm-cover').click(function() {
       $('.bm-cover').fadeOut('500');
     });
-    $('.gm-cover').click(function() {
-      $('.gm-cover').fadeOut('500');
-    });
+
+  $('.gm-cover').click(function() {
+    $('.gm-cover').fadeOut('500');
   });
 
-// PROPOSAL
-  $(document).ready(function() {
-    $(".owl-carousel").owlCarousel();   
-  });
+
+  $(".owl-carousel").owlCarousel();
+
+  //make ajax request to instagram, get JSON, dynamically build HTML
+
+	ajaxCall("https://api.instagram.com/v1/tags/happilyeverabrenica/media/recent");
 
 });
+
+
+function processResponse(data){
+	console.log(data);
+	var igFeed = data;
+
+	for(var i = 0; i < 20; i++){
+		$("#ig-feed").append("<div><a target='_blank' href='"+igFeed.data[i].link+"'><img class='ig-photo' src='"+igFeed.data[i].images.standard_resolution.url+"'/></a></div>");
+	}
+
+	// if next_url exists do another ajax call with THAT URL and then process that data
+	if (igFeed.pagination.next_url != "") {
+		ajaxCall(igFeed.pagination.next_url);
+	}
+}
+
+function ajaxCall(url){
+
+	$.ajax({
+		type: "GET",
+		url: url,
+		data: "access_token=992364.6d08390.2e471e93a5ce42eeb15ab4669a58ca11",
+		dataType: "jsonp",
+		success: function(serverResponse){
+			processResponse(serverResponse);
+		}
+	});
+}
